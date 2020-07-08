26b69cc5d1206876a0a9dc8181cd697ee85c6d73
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-07-07 22:35:05 -0400

    fixed stlying of mainly item page, added map function for ingredients

--- src/pages/ItemPage/ItemPage.js
+++ src/pages/ItemPage/ItemPage.js
@@ -12,7 +12,7 @@ class ItemPage extends Component {
     super()
 
       this.state = {
-        itemAdded: false
+        itemAdded: false,
       }
     }
 
@@ -27,7 +27,9 @@ class ItemPage extends Component {
   componentDidMount() {
       const drinkId = this.props.match.params.drinkId
 
-      const { loadItem } = this.props;
+      const { loadItem, drinkItem } = this.props;
+
+      console.log(drinkItem)
 
       fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
       .then(res => res.json())
@@ -37,115 +39,52 @@ class ItemPage extends Component {
     })
 
   }
-    
+
   render() {
   
     const { drinkItem, drinkFavorites, deleteItem } = this.props
 
     const itemMatched = drinkFavorites.find(item => item.idDrink === drinkItem.idDrink)
-    
+
+    const newValuesArray = Object.values(drinkItem)
+    const ingredientValues = newValuesArray.slice(21, 30)
+    const amountValues = newValuesArray.slice(36, 45)
+
     return (
       <div className='item-page'>
-
-        <img className='item-page-img' src={drinkItem.strDrinkThumb} alt="selected drink"/>
-        <div className="item-page-content">
-          <div className="box-title">
-            <h3 className="item-page-title">{drinkItem.strDrink}</h3>
+        <img src={drinkItem.strDrinkThumb} alt="drink" className="item-image"/>
+        <div className="content-container">
+          <div className="title-container">
+            <h1 className="drink-name">{drinkItem.strDrink}</h1>
             {
               itemMatched ?  <button value='submit' onClick={() => deleteItem(drinkItem)} className='btn btn-has far fa-thumbs-up'></button> : <button value='submit' onClick={() => this.handleClick()} className='btn'>Add to favorites</button> 
             }
           </div>
-          <div className="c-g-i-container">
-            <div className="category-glass-container">
-              <div className="section1-container">
-                <p className='label'>Category</p>
-                <p className="category">{drinkItem.strCategory}</p>
+          <div className="inner-content-container">
+            <div className="inner-left-content">
+              <div className="category-container">
+                <h3 className="category-title item-title">Category</h3>
+                <div className="category-content">{drinkItem.strCategory}</div>
               </div>
-              <div className="section1-container">
-                <p className='label'>Type of glass</p>
-                <p className="glass">{drinkItem.strGlass}</p>
+              <div className="glass-type-container">
+                <h3 className="glass-type-title item-title">Type of Glass</h3>
+                <div className="glass-type-content">{drinkItem.strGlass}</div>
               </div>
             </div>
-            <div className="instructions">
-              <p className='label'>Instructions</p>
-              <p className="intructions">{drinkItem.strInstructions}</p>
+            <div className="inner-right-content">
+              <h3 className="ingredients-title item-title">Ingredients</h3>
+                <div className="ingredients-content">
+                  {ingredientValues.map((ingredient, i) => 
+                      <div className='ingredient' key={i}>
+                        <div>{ingredient}</div>
+                        <div className='amounts'>{amountValues[i]}</div>
+                      </div>)}
+                </div>
             </div>
           </div>
-          <p className="label label-ingredients">Ingredients</p>
-          <div className="ingredients-container">
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient1}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure1}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient2}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure2}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient3}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure3}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient4}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure4}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient5}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure5}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient6}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure6}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient7}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure7}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient8}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure8}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient9}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure9}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient10}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure10}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient11}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure11}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient12}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure12}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient13}</span>
-              <br></br>
-              <span className="amount">{drinkItem.strMeasure13}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient14}</span>
-              <span className="amount">{drinkItem.strMeasure14}</span>
-            </div>
-            <div className="measurements">
-              <span className="ingredient">{drinkItem.strIngredient15}</span>
-              <span className="amount">{drinkItem.strMeasure15}</span>
-            </div>
+          <div className="description-box">
+              <h3 className="description-title item-title">Instructions</h3>
+              <div className="description-content">{drinkItem.strInstructions}</div>
           </div>
         </div>
       </div>
@@ -175,3 +114,10 @@ export default connect(mapStateToProps, mapDispatchToProps)(ItemPage)
 //     this.setState({itemAdded: false});
 //   }.bind(this),2000);
 // }
+
+// <div className="box-title">
+// <h3 className="item-page-title">{drinkItem.strDrink}</h3>
+// {
+//   itemMatched ?  <button value='submit' onClick={() => deleteItem(drinkItem)} className='btn btn-has far fa-thumbs-up'></button> : <button value='submit' onClick={() => this.handleClick()} className='btn'>Add to favorites</button> 
+// }
+// </div>
